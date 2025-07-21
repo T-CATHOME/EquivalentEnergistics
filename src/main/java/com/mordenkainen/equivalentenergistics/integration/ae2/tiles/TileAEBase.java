d1530140054f430760ab8e021499ecc4d43e789e
T_CAT_KIND <73945464+T-CATHOME@users.noreply.github.com>
2025-07-21 23:07:52 +0800

    回退 TileAEBase.java

--- src/main/java/com/mordenkainen/equivalentenergistics/integration/ae2/tiles/TileAEBase.java
+++ src/main/java/com/mordenkainen/equivalentenergistics/integration/ae2/tiles/TileAEBase.java
@@ -3,27 +3,24 @@ package com.mordenkainen.equivalentenergistics.integration.ae2.tiles;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
+
 import appeng.api.config.SecurityPermissions;
 import appeng.api.networking.security.ISecurityGrid;
 import appeng.api.networking.security.MachineSource;
 import appeng.api.util.DimensionalCoord;
+
 import com.mordenkainen.equivalentenergistics.blocks.base.tile.EqETileBase;
 import com.mordenkainen.equivalentenergistics.integration.ae2.grid.AEProxy;
 import com.mordenkainen.equivalentenergistics.integration.ae2.grid.GridAccessException;
 import com.mordenkainen.equivalentenergistics.integration.ae2.grid.GridUtils;
 import com.mordenkainen.equivalentenergistics.integration.ae2.grid.IAEProxyHost;
 import com.mordenkainen.equivalentenergistics.util.CommonUtils;
-import com.mordenkainen.equivalentenergistics.integration.ae2.cells.HandlerEMCCellBase;
-import appeng.api.storage.IMEInventoryHandler;
-import appeng.api.storage.data.IAEItemStack;
 import cpw.mods.fml.common.FMLCommonHandler;
 
 public abstract class TileAEBase extends EqETileBase implements IAEProxyHost {
 
     private final static String POWERED_TAG = "powered";
     private final static String ACTIVE_TAG = "active";
-    private final static String EE_CELL_DATA = "EE_CellData";
 
     protected final AEProxy gridProxy;
     protected MachineSource mySource;
@@ -63,57 +60,13 @@ public abstract class TileAEBase extends EqETileBase implements IAEProxyHost {
     public void readFromNBT(final NBTTagCompound data) {
         super.readFromNBT(data);
         IAEProxyHost.super.readFromNBT(data);
-
-        // ----------- 读取 EMC 存储单元状态 -----------
-        if (data.hasKey(EE_CELL_DATA)) {
-            NBTTagList cellList = data.getTagList(EE_CELL_DATA, 10); // 10 = NBTTagCompound
-            for (int i = 0; i < cellList.tagCount(); i++) {
-                NBTTagCompound cellData = cellList.getCompoundTagAt(i);
-                int slot = cellData.getInteger("Slot");
-                IMEInventoryHandler<IAEItemStack> handler = getCellHandlerSafe(slot);
-                if (handler instanceof HandlerEMCCellBase) {
-                    ((HandlerEMCCellBase) handler).readFromNBT(cellData);
-                }
-            }
-        }
     }
 
     @Override
     public void writeToNBT(final NBTTagCompound data) {
         super.writeToNBT(data);
         IAEProxyHost.super.writeToNBT(data);
-
-        // ----------- 保存 EMC 存储单元状态 -----------
-        NBTTagList cellList = new NBTTagList();
-        int cellCount = getCellCountSafe();
-        for (int i = 0; i < cellCount; i++) {
-            IMEInventoryHandler<IAEItemStack> handler = getCellHandlerSafe(i);
-            if (handler instanceof HandlerEMCCellBase) {
-                NBTTagCompound cellData = new NBTTagCompound();
-                cellData.setInteger("Slot", i);
-                ((HandlerEMCCellBase) handler).writeToNBT(cellData);
-                cellList.appendTag(cellData);
-            }
-        }
-        if (cellList.tagCount() > 0) {
-            data.setTag(EE_CELL_DATA, cellList);
-        }
     }
-
-    // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-    // 你需要在实际的驱动器类(比如 TileEMCDrive 或 EMC存储箱的TileEntity)中实现以下两个方法。
-    // 例如，如果你的驱动器有10个槽位，并能通过 inv.getCellHandler(i) 获取 handler，可以：
-    // @Override
-    // protected int getCellCountSafe() { return 10; }
-    // @Override
-    // protected IMEInventoryHandler<IAEItemStack> getCellHandlerSafe(int slot) { return inv.getCellHandler(slot); }
-    protected int getCellCountSafe() {
-        return 0;
-    }
-    protected IMEInventoryHandler<IAEItemStack> getCellHandlerSafe(int slot) {
-        return null;
-    }
-    // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
     @Override
     public AEProxy getProxy() {
@@ -197,4 +150,5 @@ public abstract class TileAEBase extends EqETileBase implements IAEProxyHost {
         }
         return flag;
     }
+
 }
