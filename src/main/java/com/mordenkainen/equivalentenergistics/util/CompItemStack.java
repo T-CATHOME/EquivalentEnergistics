f9dac6487bc9db791fe894fad309fd22a570601e
T_CAT_KIND <73945464+T-CATHOME@users.noreply.github.com>
2025-07-22 00:14:17 +0800

    Update CompItemStack.java

--- src/main/java/com/mordenkainen/equivalentenergistics/util/CompItemStack.java
+++ src/main/java/com/mordenkainen/equivalentenergistics/util/CompItemStack.java
@@ -14,23 +14,20 @@ public class CompItemStack extends Equivalence<ItemStack> {
         if (stack1 == null) {
             return stack2 == null;
         }
-
         if (stack2 == null) {
             return false;
         }
-
+        // 只比较物品，不比较 meta/damage
         if (!stack1.getItem().equals(stack2.getItem())) {
             return false;
         }
-
-        if (stack1.getItemDamage() != stack2.getItemDamage()) {
-            return false;
-        }
-
+        // 注释掉 meta 检查，使所有 meta 的 EMC Crystal 视为等价
+        // if (stack1.getItemDamage() != stack2.getItemDamage()) {
+        //     return false;
+        // }
         if (!stack1.hasTagCompound()) {
             return !stack2.hasTagCompound();
         }
-
         return stack1.stackTagCompound.equals(stack2.stackTagCompound);
     }
 
@@ -38,9 +35,9 @@ public class CompItemStack extends Equivalence<ItemStack> {
     protected int doHash(final ItemStack stack) {
         int result = 1;
         result = PRIME * result + Item.getIdFromItem(stack.getItem());
-        result = PRIME * result + stack.getItemDamage();
+        // 注释掉 meta 的 hash，使所有 meta 的 EMC Crystal hash 相同
+        // result = PRIME * result + stack.getItemDamage();
         result = PRIME * result + (stack.hasTagCompound() ? stack.stackTagCompound.hashCode() : 0);
         return result;
     }
-
 }
